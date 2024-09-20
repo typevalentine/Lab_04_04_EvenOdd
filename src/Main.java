89b34f6fe7dbe8db963846ac53229fcb38d428c5
typevalentine <koilui7112@gmail.com>
2024-09-20 10:08:09 -0400

    Completed Task

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        int numToExamine = 7;
+        int modulo = 0;
+        modulo = numToExamine % 2;
+        System.out.println("If output is 0 it is even, if the output is 1 then it is odd. The output is: " + modulo);
+    }
+}
